3b09ba2e521483c619c6a7bc5aab2bec3c43c646
atari-monk <atari.monk1@gmail.com>
2023-04-13 19:06:57 +0200

    step 2

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ class Player {
 
   // Draw the player rectangle on the canvas
   draw(ctx) {
+    ctx.fillStyle = "red";
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 
@@ -35,7 +36,22 @@ class Player {
 // Initialize the canvas and player
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const player = new Player(50, 50, 50, 50);
+const player = new Player(
+  canvas.width / 2,
+  canvas.height / 2,
+  canvas.width * 0.1,
+  canvas.height * 0.1
+);
+
+// Set canvas size based on viewport
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight * 0.9;
+  player.width = canvas.width * 0.1;
+  player.height = canvas.height * 0.1;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
 
 // Listen for keyboard input and move the player accordingly
 document.addEventListener("keydown", (event) => {
