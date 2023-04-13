dbb0b2fa634aceddbdce4a265a83ed7c095e80a8
atari-monk <atari.monk1@gmail.com>
2023-04-13 23:28:27 +0200

    Answear 3

---
+++ session2/Game.js
@@ -0,0 +1,40 @@
+export class Game {
+  constructor() {
+    this.canvas = document.getElementById("game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.isRunning = false;
+
+    // Bind event listeners to buttons
+    this.startBtn = document.getElementById("start-btn");
+    this.pauseBtn = document.getElementById("pause-btn");
+    this.resetBtn = document.getElementById("reset-btn");
+    this.startBtn.addEventListener("click", this.start.bind(this));
+    this.pauseBtn.addEventListener("click", this.pause.bind(this));
+    this.resetBtn.addEventListener("click", this.reset.bind(this));
+  }
+
+  start() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.loop();
+    }
+  }
+
+  pause() {
+    this.isRunning = false;
+  }
+
+  reset() {
+    // Reset game state here
+  }
+
+  loop() {
+    if (!this.isRunning) {
+      return;
+    }
+
+    // Update game state here
+    // Draw game graphics here
+    requestAnimationFrame(this.loop.bind(this));
+  }
+}
