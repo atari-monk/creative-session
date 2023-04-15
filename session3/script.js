64efe9f2e8ae16cc2977fcac993a8f1e35b7d62c
atari-monk <atari.monk1@gmail.com>
2023-04-15 15:31:20 +0200

    overlay

--- session3/script.js
+++ session3/script.js
@@ -1,3 +1,16 @@
 const canvas = document.getElementById("my-canvas");
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
+
+const button = document.getElementById("small-button");
+const overlay = document.getElementById("overlay");
+
+button.addEventListener("click", () => {
+  overlay.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target === overlay || event.target.parentElement === overlay) {
+    overlay.style.display = "none";
+  }
+});
