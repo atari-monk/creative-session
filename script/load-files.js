44a7fdfa29df887b642e6a8daef34a014883aa03
atari-monk <atari.monk1@gmail.com>
2023-04-18 21:24:27 +0200

    upd script

--- script/load-files.js
+++ script/load-files.js
@@ -11,26 +11,67 @@ const outputDirectory = "./";
 const outputFile = "output.txt";
 
 // The file extensions to include
-const fileExtensions = [".js", ".html", ".css"];
+const fileExtensions = [".html", ".css", ".js"];
 
 // Read all files in the directory with the given extensions
 fs.readdir(directoryPath, (err, files) => {
   if (err) throw err;
 
-  const fileContents = [];
+  const htmlFileContents = [];
+  const cssFileContents = [];
+  const jsFileContents = [];
+  const scriptFileContents = [];
 
   files.forEach((file) => {
     const fileExt = path.extname(file);
-    if (fileExtensions.includes(fileExt)) {
+    if (fileExtensions.includes(fileExt) && file !== "script.js") {
       const filePath = path.join(directoryPath, file);
       const content = fs.readFileSync(filePath, "utf8");
-      fileContents.push(content);
+      if (fileExt === ".html") {
+        htmlFileContents.push({ name: file, content });
+      } else if (fileExt === ".css") {
+        cssFileContents.push({ name: file, content });
+      } else {
+        jsFileContents.push({ name: file, content });
+      }
+    } else if (file === "script.js") {
+      const filePath = path.join(directoryPath, file);
+      const content = fs.readFileSync(filePath, "utf8");
+      scriptFileContents.push(content);
     }
   });
 
+  // Sort the HTML and CSS file contents alphabetically by name
+  htmlFileContents.sort((a, b) => a.name.localeCompare(b.name));
+  cssFileContents.sort((a, b) => a.name.localeCompare(b.name));
+
+  // Combine the file contents in order with a newline and header between each file
+  const outputContent =
+    htmlFileContents
+      .map(
+        (file) =>
+          `/*------------------------------${file.name}------------------------------*/\n${file.content}\n`
+      )
+      .join("") +
+    cssFileContents
+      .map(
+        (file) =>
+          `/*------------------------------${file.name}------------------------------*/\n${file.content}\n`
+      )
+      .join("") +
+    jsFileContents
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map(
+        (file) =>
+          `/*------------------------------${file.name}------------------------------*/\n${file.content}\n`
+      )
+      .join("") +
+    `/*------------------------------script.js------------------------------*/\n${scriptFileContents.join(
+      "\n"
+    )}`;
+
   // Write the file contents to the output file
   const outputFilePath = path.join(outputDirectory, outputFile);
-  const outputContent = fileContents.join("\n");
   fs.writeFile(outputFilePath, outputContent, (err) => {
     if (err) throw err;
     console.log(`File written to ${outputFilePath}`);
