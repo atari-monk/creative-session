806ec1c77c2041009a6e2a914f39f9bdef76dad4
atari-monk <atari.monk1@gmail.com>
2023-04-29 21:44:32 +0200

    fix hide offer

--- chat-browser/src/js/script.js
+++ chat-browser/src/js/script.js
@@ -1,36 +1,49 @@
 const p = new window.SimplePeer({
-  initiator: location.hash === "#1",
+  initiator: location.hash === '#1',
   trickle: false,
 });
 
-p.on("error", (err) => console.log("error", err));
+p.on('error', (err) => console.log('error', err));
 
-p.on("signal", (data) => {
-  console.log("SIGNAL", JSON.stringify(data));
-  document.querySelector("#outgoing").textContent = JSON.stringify(data);
+p.on('signal', (data) => {
+  console.log('SIGNAL', JSON.stringify(data));
+  document.querySelector('#outgoing').textContent =
+    JSON.stringify(data);
 });
 
-document.querySelector("form").addEventListener("submit", (ev) => {
-  ev.preventDefault();
-  p.signal(JSON.parse(document.querySelector("#connect-msg").value));
-});
+document
+  .querySelector('#connect-btn')
+  .addEventListener('click', (ev) => {
+    p.signal(
+      JSON.parse(document.querySelector('#connect-msg').value)
+    );
+    document.querySelector('#connect-msg').value = '';
+  });
 
-p.on("connect", () => {
-  console.log("CONNECT");
-  p.send("whatever" + Math.random());
+p.on('connect', () => {
+  console.log('CONNECT');
+  p.send('whatever' + Math.random());
 
   // send message
-  document.querySelector("#send-btn").addEventListener("click", (ev) => {
-    const msg = document.querySelector("#outgoing-msg").value;
-    p.send(msg);
-    document.querySelector("#outgoing-msg").value = "";
-  });
+  document
+    .querySelector('#send-btn')
+    .addEventListener('click', (ev) => {
+      const msg = document.querySelector('#outgoing-msg').value;
+      p.send(msg);
+      document.querySelector('#outgoing-msg').value = '';
+    });
 });
 
-p.on("data", (data) => {
-  console.log("data: " + data);
+p.on('data', (data) => {
+  console.log('data: ' + data);
   // display incoming message
-  const pre = document.createElement("pre");
+  const pre = document.createElement('pre');
   pre.textContent = data;
-  document.querySelector("#outgoing").appendChild(pre);
+  document.querySelector('#outgoing').appendChild(pre);
 });
+
+if (window.location.href.includes('#1')) {
+  document.getElementById('outgoing').classList.remove('hide');
+} else {
+  document.getElementById('outgoing').classList.add('hide');
+}
