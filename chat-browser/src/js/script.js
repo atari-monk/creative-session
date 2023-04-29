0ac83e45223dd70d7219e93d72601d029484ceda
atari-monk <atari.monk1@gmail.com>
2023-04-29 20:04:34 +0200

    separete concerns to files

---
+++ chat-browser/src/js/script.js
@@ -0,0 +1,36 @@
+const p = new window.SimplePeer({
+  initiator: location.hash === "#1",
+  trickle: false,
+});
+
+p.on("error", (err) => console.log("error", err));
+
+p.on("signal", (data) => {
+  console.log("SIGNAL", JSON.stringify(data));
+  document.querySelector("#outgoing").textContent = JSON.stringify(data);
+});
+
+document.querySelector("form").addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  p.signal(JSON.parse(document.querySelector("#incoming").value));
+});
+
+p.on("connect", () => {
+  console.log("CONNECT");
+  p.send("whatever" + Math.random());
+
+  // send message
+  document.querySelector("#send-btn").addEventListener("click", (ev) => {
+    const msg = document.querySelector("#outgoing-msg").value;
+    p.send(msg);
+    document.querySelector("#outgoing-msg").value = "";
+  });
+});
+
+p.on("data", (data) => {
+  console.log("data: " + data);
+  // display incoming message
+  const pre = document.createElement("pre");
+  pre.textContent = data;
+  document.querySelector("#outgoing").appendChild(pre);
+});
