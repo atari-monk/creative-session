ef1db8753c373bec652aa4871c61c703a339a266
atari-monk <atari.monk1@gmail.com>
2023-04-30 16:49:21 +0200

    PeerChatUI class

--- peer-to-peer-chat/src/js/peer-chat-ui.js
+++ peer-to-peer-chat/src/js/peer-chat-ui.js
@@ -1,10 +1,13 @@
 export class PeerChatUI {
   constructor() {
+    this.sendOnEnter = true;
     // UI elements
     this.toggleEnterBehaviorButton = document.querySelector(
       '#toggle-enter-behavior'
     );
     this.chatContainer = document.querySelector('.chat');
+    this.connectContainer = document.querySelector('.connect');
+    this.chatContainer.style.display = 'none';
     this.peerIdInput = document.querySelector('#peer-id');
     this.instructionText = document.querySelector('#instruction');
     this.initiatorIdText = document.querySelector('#initiator-id');
@@ -44,12 +47,18 @@ export class PeerChatUI {
   handleToggleEnterBehaviorButtonClick = (ev) => {
     // toggle sendOnEnter behavior
     // update button text
+    this.sendOnEnter = !this.sendOnEnter; // toggle the behavior
+    ev.target.textContent = this.sendOnEnter
+      ? 'Send on Enter'
+      : 'New line on Enter'; // update the button text
   };
 
   handleConnectButtonClick = (ev) => {
     // get peer ID from input field
     // signal peer
     // clear input field
+    this.peer.signal(JSON.parse(this.peerIdInput.value));
+    this.peerIdInput.value = '';
   };
 
   handleSendButtonClick = (ev) => {
@@ -57,6 +66,13 @@ export class PeerChatUI {
     // send message through peer object
     // clear input field
     // display message in chat text container
+    const msg = this.outgoingMsgInput.value;
+    this.peer.send(msg);
+    this.outgoingMsgInput.value = '';
+
+    const pre = document.createElement('pre');
+    pre.textContent = `You: ${msg}`;
+    this.chatTextContainer.appendChild(pre);
   };
 
   handleOutgoingMsgInputKeyDown = (ev) => {
@@ -65,28 +81,54 @@ export class PeerChatUI {
     // send message through peer object
     // clear input field
     // display message in chat text container
+    if (ev.key === 'Enter' && this.sendOnEnter) {
+      ev.preventDefault();
+      const msg = this.outgoingMsgInput.value;
+      this.outgoingMsgInput.value = '';
+
+      const pre = document.createElement('pre');
+      pre.textContent = `You: ${msg}`;
+      pre.classList.add('your-msg');
+
+      this.chatTextContainer.appendChild(pre);
+      this.peer.send(msg);
+    }
   };
 
   handleCopyButtonClick = (ev) => {
     // copy offer connection data to clipboard
     // update UI to indicate that data has been copied
+    const offerEl = this.initiatorIdText;
+    const connectData = offerEl.textContent;
+    navigator.clipboard.writeText(connectData).then(() => {
+      offerEl.classList.add('initiator-id-copied');
+      offerEl.textContent = 'Copied';
+    });
   };
 
   // peer object event listeners
   handlePeerError = (err) => {
     // display error message in UI
+    window.document.body.textContent = err.message;
   };
 
   handlePeerSignal = (data) => {
     // display connection data in UI
+    this.initiatorIdText.textContent = JSON.stringify(data, null, 2);
   };
 
   handlePeerConnect = () => {
     // hide connection UI
     // display chat UI
+    this.connectContainer.style.display = 'none';
+    this.chatContainer.style.display = 'flex';
   };
 
   handlePeerData = (data) => {
     // display received message in chat text container
+    const pre = document.createElement('pre');
+    pre.textContent = `Peer: ${data}`;
+    pre.classList.add('peer-msg');
+    this.chatTextContainer.appendChild(pre);
   };
 }
