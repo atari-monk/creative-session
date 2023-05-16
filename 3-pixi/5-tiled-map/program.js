930cca282c252f4f8547d21b9a0a01985568f871
atari-monk <atari.monk1@gmail.com>
2023-05-16 19:26:37 +0200

    tiled

---
+++ 3-pixi/5-tiled-map/program.js
@@ -0,0 +1,12 @@
+import { App } from '../pixi-lib/App.js';
+
+const option = {
+  canvasId: 'mainCanvasId',
+  backgroundColor: 0x000000,
+  width: 800,
+  height: 600,
+  fullScreen: true,
+  log: true,
+};
+
+const app = new App(option);
