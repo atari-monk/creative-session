13127026be8283f306599d28df8f5b157c8342d1
atari-monk <atari.monk1@gmail.com>
2023-05-19 13:02:51 +0200

    refactor robot

--- 3-pixi/1-robot/Robot.js
+++ 3-pixi/1-robot/Robot.js
@@ -2,87 +2,124 @@ import { GameObject } from './../pixi-lib/GameObject.js';
 
 export class Robot extends GameObject {
   #pixiApp;
+  #container;
+  #head;
+  #torso;
+  #leftArmJoint;
+  #rightArmJoint;
+  #leftLegJoint;
+  #rightLegJoint;
+  #leftArm;
+  #rightArm;
+  #leftLeg;
+  #rightLeg;
+  #robotX;
+  #robotDirection;
 
   constructor(pixiApp) {
     super();
     this.#pixiApp = pixiApp;
-    this.container = new PIXI.Container();
-    this.robotX = 0;
-    this.robotDirection = 1;
+    this.#container = new PIXI.Container();
+    this.#robotX = 0;
+    this.#robotDirection = 1;
 
     this.#createBodyParts();
     this.#setInitialPositions();
   }
 
   #createBodyParts() {
-    this.head = PIXI.Sprite.from('./assets/head.png');
-    this.torso = PIXI.Sprite.from('./assets/torso.png');
+    this.#createHead();
+    this.#createTorso();
+    this.#createJointContainers();
+    this.#createArmAndLegSprites();
+    this.#addSpritesToContainers();
+    this.#addContainersToTorso();
+    this.#addBodyPartsToContainer();
+  }
 
-    // Create the joint sprites for the arms and legs
-    this.leftArmJoint = new PIXI.Container();
-    this.rightArmJoint = new PIXI.Container();
-    this.leftLegJoint = new PIXI.Container();
-    this.rightLegJoint = new PIXI.Container();
+  #createHead() {
+    this.#head = PIXI.Sprite.from('./assets/head.png');
+  }
 
-    // Create the arm and leg sprites
-    this.leftArm = PIXI.Sprite.from('./assets/left-arm.png');
-    this.rightArm = PIXI.Sprite.from('./assets/right-arm.png');
-    this.leftLeg = PIXI.Sprite.from('./assets/left-leg.png');
-    this.rightLeg = PIXI.Sprite.from('./assets/right-leg.png');
+  #createTorso() {
+    this.#torso = PIXI.Sprite.from('./assets/torso.png');
+  }
 
-    // Add the arm and leg sprites to the joint containers
-    this.leftArmJoint.addChild(this.leftArm);
-    this.rightArmJoint.addChild(this.rightArm);
-    this.leftLegJoint.addChild(this.leftLeg);
-    this.rightLegJoint.addChild(this.rightLeg);
+  #createJointContainers() {
+    this.#leftArmJoint = new PIXI.Container();
+    this.#rightArmJoint = new PIXI.Container();
+    this.#leftLegJoint = new PIXI.Container();
+    this.#rightLegJoint = new PIXI.Container();
+  }
 
-    // Add the joint containers to the torso
-    this.torso.addChild(
-      this.leftArmJoint,
-      this.rightArmJoint,
-      this.leftLegJoint,
-      this.rightLegJoint
+  #createArmAndLegSprites() {
+    this.#leftArm = PIXI.Sprite.from('./assets/left-arm.png');
+    this.#rightArm = PIXI.Sprite.from('./assets/right-arm.png');
+    this.#leftLeg = PIXI.Sprite.from('./assets/left-leg.png');
+    this.#rightLeg = PIXI.Sprite.from('./assets/right-leg.png');
+  }
+
+  #addSpritesToContainers() {
+    this.#leftArmJoint.addChild(this.#leftArm);
+    this.#rightArmJoint.addChild(this.#rightArm);
+    this.#leftLegJoint.addChild(this.#leftLeg);
+    this.#rightLegJoint.addChild(this.#rightLeg);
+  }
+
+  #addContainersToTorso() {
+    this.#torso.addChild(
+      this.#leftArmJoint,
+      this.#rightArmJoint,
+      this.#leftLegJoint,
+      this.#rightLegJoint
     );
+  }
 
-    // Add the body parts to the main container
-    this.container.addChild(this.head, this.torso);
+  #addBodyPartsToContainer() {
+    this.#container.addChild(this.#head, this.#torso);
   }
 
   #setInitialPositions() {
-    this.head.position.set(160, 50);
-    this.torso.position.set(90, 120);
-    this.leftArmJoint.position.set(-20, 20);
-    this.rightArmJoint.position.set(140, 20);
-    this.leftLegJoint.position.set(25, 180);
-    this.rightLegJoint.position.set(80, 170);
+    this.#head.position.set(160, 50);
+    this.#torso.position.set(90, 120);
+    this.#leftArmJoint.position.set(-20, 20);
+    this.#rightArmJoint.position.set(140, 20);
+    this.#leftLegJoint.position.set(25, 180);
+    this.#rightLegJoint.position.set(80, 170);
   }
 
   draw(stage) {
-    this.#pixiApp.stage.addChild(this.container);
+    this.#pixiApp.stage.addChild(this.#container);
   }
 
   update(deltaTime) {
+    this.#updateRobotPosition();
+    this.#rotateArmsAndLegs();
+    this.#checkBoundaries();
+  }
+
+  #updateRobotPosition() {
     const time = this.#pixiApp.ticker.lastTime;
-    this.robotX += 1 * this.robotDirection;
+    this.#robotX += 1 * this.#robotDirection;
+    this.#container.position.x = this.#robotX;
+  }
 
-
-    // Update the position of the robot container
-    this.container.position.x = this.robotX;
-
-    // Rotate the arms and legs relative to the torso
+  #rotateArmsAndLegs() {
+    const time = this.#pixiApp.ticker.lastTime;
     const armRotation = Math.sin(time / 100) * 0.05;
     const legRotation = -Math.sin(time / 100) * 0.1;
+    this.#leftArmJoint.rotation = armRotation;
+    this.#rightArmJoint.rotation = -armRotation;
+    this.#leftLegJoint.rotation = legRotation;
+    this.#rightLegJoint.rotation = -legRotation;
+  }
 
-    this.leftArmJoint.rotation = armRotation;
-    this.rightArmJoint.rotation = -armRotation;
-    this.leftLegJoint.rotation = legRotation;
-    this.rightLegJoint.rotation = -legRotation;
-
+  #checkBoundaries() {
     if (
-      this.robotX >= this.#pixiApp.renderer.width - this.container.width ||
-      this.robotX <= 0
+      this.#robotX >= this.#pixiApp.renderer.width - this.#container.width ||
+      this.#robotX <= 0
     ) {
-      this.robotDirection *= -1;
+      this.#robotDirection *= -1;
     }
   }
 }
