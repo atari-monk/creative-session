6a6f3232552d886b7f87829c6ddd6f2c71613059
atari-monk <atari.monk1@gmail.com>
2023-05-21 16:44:27 +0200

    create privates

--- 3-pixi/pixi-lib/server/server.js
+++ 3-pixi/pixi-lib/server/server.js
@@ -5,83 +5,90 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 class GameServer {
+  #app;
+  #server;
+  #ioOptions;
+  #io;
+  #PORT;
+  #clients;
+
   constructor() {
-    this.app = express();
-    this.server = http.createServer(this.app);
-    this.ioOptions = {
+    this.#app = express();
+    this.#server = http.createServer(this.#app);
+    this.#ioOptions = {
       cors: {
         origin: '*',
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type'],
       },
     };
-    this.io = socketIO(this.server, this.ioOptions);
-    this.PORT = process.env.PORT || 3000;
-    this.clients = {};
+    this.#io = socketIO(this.#server, this.#ioOptions);
+    this.#PORT = process.env.PORT || 3000;
+    this.#clients = {};
   }
 
   start() {
-    this.configureMiddleware();
-    this.configureSocketIO();
-    this.listen();
+    this.#configureMiddleware();
+    this.#configureSocketIO();
+    this.#listen();
   }
 
-  configureMiddleware() {
-    this.app.use(cors());
+  #configureMiddleware() {
+    this.#app.use(cors());
   }
 
-  configureSocketIO() {
-    this.io.on('connection', (socket) => {
-      this.handleClientConnection(socket);
+  #configureSocketIO() {
+    this.#io.on('connection', (socket) => {
+      this.#handleClientConnection(socket);
     });
   }
 
-  handleClientConnection(socket) {
+  #handleClientConnection(socket) {
     console.log('A user connected');
 
-    const clientId = this.generateClientId(socket);
-    this.storeClient(clientId, socket);
-    this.handleClientDisconnection(clientId);
-    this.handlePlayerMovement(socket);
+    const clientId = this.#generateClientId(socket);
+    this.#storeClient(clientId, socket);
+    this.#handleClientDisconnection(clientId);
+    this.#handlePlayerMovement(socket);
 
-    const clientIdList = this.getClientIdList();
-    this.emitClientIdList(clientIdList);
+    const clientIdList = this.#getClientIdList();
+    this.#emitClientIdList(clientIdList);
 
     // Other game-related events and logic can be implemented here
   }
 
-  generateClientId(socket) {
+  #generateClientId(socket) {
     return socket.id;
   }
 
-  storeClient(clientId, socket) {
-    this.clients[clientId] = { socket };
+  #storeClient(clientId, socket) {
+    this.#clients[clientId] = { socket };
   }
 
-  handleClientDisconnection(clientId) {
-    this.clients[clientId].socket.on('disconnect', () => {
+  #handleClientDisconnection(clientId) {
+    this.#clients[clientId].socket.on('disconnect', () => {
       console.log('A user disconnected');
-      delete this.clients[clientId];
+      delete this.#clients[clientId];
     });
   }
 
-  handlePlayerMovement(socket) {
+  #handlePlayerMovement(socket) {
     socket.on('movement', ({ clientId, newPosition }) => {
       socket.broadcast.emit('movement', { clientId, newPosition });
     });
   }
 
-  getClientIdList() {
-    return Object.keys(this.clients);
+  #getClientIdList() {
+    return Object.keys(this.#clients);
   }
 
-  emitClientIdList(clientIdList) {
-    this.io.emit('clientIdList', clientIdList);
+  #emitClientIdList(clientIdList) {
+    this.#io.emit('clientIdList', clientIdList);
   }
 
-  listen() {
-    this.server.listen(this.PORT, () => {
-      console.log(`Server is running on port ${this.PORT}`);
+  #listen() {
+    this.#server.listen(this.#PORT, () => {
+      console.log(`Server is running on port ${this.#PORT}`);
     });
   }
 }
