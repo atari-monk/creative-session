2ff5ebf5156a9942d1475e8cb61e112cde5d235a
atari-monk <atari.monk1@gmail.com>
2023-06-09 22:46:19 +0200

    refactor SRP

--- 4-pixi/5-client/src/BallSocket.ts
+++ 4-pixi/5-client/src/BallSocket.ts
@@ -1,16 +1,12 @@
-import { EventEmitter } from 'eventemitter3';
-import { VectorData } from '../../2-pixi-lib/dist/VectorData.js';
 import { SocketErrorHandler } from './SocketErrorHandler.js';
 
-export class BallClient {
+export class BallSocket {
   private ballObj: any = null;
   private readonly socketConnection: SocketErrorHandler;
-  protected readonly _emitter: EventEmitter;
 
-  constructor(socketConnection: SocketErrorHandler, emitter: EventEmitter) {
-    this.socketConnection = socketConnection;
-    this._emitter = emitter;
+  constructor(socketConnection: SocketErrorHandler) {
     this.ballObj = null;
+    this.socketConnection = socketConnection;
     this.socketConnection.socket.on(
       'ballMovement',
       this.handleBallMovement.bind(this)
@@ -19,24 +15,6 @@ export class BallClient {
       'ballVelocity',
       this.handleBallVelocity.bind(this)
     );
-    this._emitter.on('ball-pos-upd', this.emittBallPosition.bind(this));
-    this._emitter.on('ball-vel-upd', this.emittBallVelocity.bind(this));
-  }
-
-  private emittBallPosition(data: VectorData) {
-    this.socketConnection.socket.emit('ballMovement', data.newVector);
-  }
-
-  private emittBallVelocity(data: VectorData) {
-    this.socketConnection.socket.emit('ballVelocity', data.newVector);
-  }
-
-  public addBallObj(ball: any) {
-    this.ballObj = ball;
-  }
-
-  public removeBallObj() {
-    this.ballObj = null;
   }
 
   private handleBallMovement(newPosition: { x: number; y: number }) {
@@ -61,5 +39,13 @@ export class BallClient {
       throw new Error('No ball object added!');
     }
     this.ballObj.velocity = newVelocity;
+  }
+
+  public addBallObj(ball: any) {
+    this.ballObj = ball;
+  }
+
+  public removeBallObj() {
+    this.ballObj = null;
   }
 }
