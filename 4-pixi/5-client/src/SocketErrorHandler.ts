aa27292d53e9c6a863d50c6f999e16254778e5b0
atari-monk <atari.monk1@gmail.com>
2023-06-12 17:04:42 +0200

    rm prop

--- 4-pixi/5-client/src/SocketErrorHandler.ts
+++ 4-pixi/5-client/src/SocketErrorHandler.ts
@@ -1,27 +1,21 @@
-import { Socket, connect } from 'socket.io-client';
+import { Socket } from 'socket.io-client';
 
 export class SocketErrorHandler {
-  private _socket: Socket;
-
-  public get socket() {
-    return this._socket;
-  }
-
-  constructor(socket: Socket) {
-    this._socket = socket;
+  constructor(private readonly socket: Socket) {
+    this.socket = socket;
     this.setupErrorHandling();
   }
 
   private setupErrorHandling() {
-    this._socket.on('connect_error', (error: Error) => {
+    this.socket.on('connect_error', (error: Error) => {
       console.error('Socket connection error:', error);
     });
 
-    this._socket.on('connect_timeout', (timeout: number) => {
+    this.socket.on('connect_timeout', (timeout: number) => {
       console.error('Socket connection timeout:', timeout);
     });
 
-    this._socket.on('error', (error: Error) => {
+    this.socket.on('error', (error: Error) => {
       console.error('Socket error:', error);
     });
   }
