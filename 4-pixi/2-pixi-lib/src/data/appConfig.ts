ee91754a9b738e05fdb7c20dee5504d5300b8878
atari-monk <atari.monk1@gmail.com>
2023-06-20 14:11:52 +0200

    handle server side bug

--- 4-pixi/2-pixi-lib/src/data/appConfig.ts
+++ 4-pixi/2-pixi-lib/src/data/appConfig.ts
@@ -7,12 +7,20 @@ import {
   IBallOptions,
 } from './configTypes';
 
-const urlParams = new URLSearchParams(window.location.search);
-const playerUrlParam = urlParams.get('player');
-if (playerUrlParam !== '1' && playerUrlParam !== '2') {
-  throw new Error(
-    'Invalid player URL parameter. Please specify either "1" or "2".'
+let playerUrlParam: string | null = '';
+try {
+  const urlParams = new URLSearchParams(window.location.search);
+  playerUrlParam = urlParams.get('player');
+  if (playerUrlParam !== '1' && playerUrlParam !== '2') {
+    throw new Error(
+      'Invalid player URL parameter. Please specify either "1" or "2".'
+    );
+  }
+} catch (error) {
+  console.error(
+    'Error: Invalid player URL parameter. Defaulting to empty string.'
   );
+  playerUrlParam = '';
 }
 
 export const keys: IKeys = {
