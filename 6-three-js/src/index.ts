bc09e5620865ae58bd2d2f355c8c2e7a180433fc
atari-monk <atari.monk1@gmail.com>
2023-06-24 23:19:40 +0200

    three-js-intro

---
+++ 6-three-js/src/index.ts
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(
+  75, // field of view
+  window.innerWidth / window.innerHeight, // aspect ratio
+  0.1, // near clipping plane
+  1000 // far clipping plane
+);
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create a geometry
+const geometry = new THREE.BoxGeometry();
+
+// Create a material
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+// Create a mesh
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Position the camera
+camera.position.z = 5;
+
+// Create an animation loop
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Rotate the cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // Render the scene with the camera
+  renderer.render(scene, camera);
+}
+
+// Start the animation loop
+animate();
