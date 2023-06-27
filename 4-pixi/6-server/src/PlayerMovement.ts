b83660e3488ac4c9ccd54d4700d93596d7cb5e31
atari-monk <atari.monk1@gmail.com>
2023-06-27 21:32:15 +0200

    upd server

--- 4-pixi/6-server/src/PlayerMovement.ts
+++ 4-pixi/6-server/src/PlayerMovement.ts
@@ -1,11 +1,15 @@
 import { Socket } from 'socket.io';
-import { SrvSctLogicUnit } from 'atari-monk-pixi-lib';
+import { IVectorData, SrvSctLogicUnit } from 'atari-monk-pixi-lib';
 
 export class PlayerMovement extends SrvSctLogicUnit {
-  protected logicUnit(
-    socket: Socket,
-    newPosition: { x: number; y: number }
-  ): void {
-    socket.broadcast.emit('movement', newPosition);
+  protected logicUnit(socket: Socket, data: IVectorData): void {
+    const jsObj = {
+      clientId: data.clientId,
+      newVector: {
+        x: data.newVector.x,
+        y: data.newVector.y,
+      },
+    };
+    socket.broadcast.emit('movement', jsObj);
   }
 }
