86d74cf7e6673d37bc15edf93aacbee4d2731bb0
atari-monk <atari.monk1@gmail.com>
2023-06-29 20:55:43 +0200

    upd player factory v2

--- 4-pixi/8-ball/src/V2/PlayersFactory.ts
+++ 4-pixi/8-ball/src/V2/PlayersFactory.ts
@@ -1,30 +1,17 @@
 import { EventEmitter } from 'eventemitter3';
 import {
-  keys,
   SharedPlayerFactory,
   RivalPlayerFactory,
   PlayerFactory,
   IPlayer,
 } from 'atari-monk-pixi-lib';
-import {
-  KeyboardInputV1,
-  KeyboardInputHandler,
-  PlayerObject,
-  BasicRenderer,
-  PositionEmitter,
-  PlayerComputation,
-} from 'atari-monk-pixi-lib';
 import { AppFactory } from './AppFactory';
 import { Container } from 'inversify';
-import { IPlayerOptionsV2 } from 'atari-monk-pixi-lib/data/configTypes';
 
 export class PlayersFactory {
   private _emitter: EventEmitter;
   private _player1: IPlayer;
   private _player2: IPlayer;
-  private keyboard: KeyboardInputHandler;
-  private positionEmitter: PositionEmitter;
-  private playerRenderer: BasicRenderer;
 
   public get emitter() {
     return this._emitter;
@@ -39,15 +26,6 @@ export class PlayersFactory {
   }
 
   constructor() {
-    this._emitter = new EventEmitter();
-    this.positionEmitter = new PositionEmitter(
-      'position-update',
-      this._emitter
-    );
-    this.playerRenderer = new BasicRenderer();
-    this.keyboard = new KeyboardInputHandler(new KeyboardInputV1(), keys);
-    //this._player1 = this.createPlayer(playerParams, -250);
-
     const container = new Container();
     const sharedPlayerFactory = new SharedPlayerFactory(container);
     const rivalPlayerFactory = new RivalPlayerFactory(container);
@@ -55,23 +33,9 @@ export class PlayersFactory {
     sharedPlayerFactory.registerDependencies();
     rivalPlayerFactory.registerDependencies();
     playerFactory.registerDependencies();
+    this._emitter = container.resolve<EventEmitter>(EventEmitter);
     this._player1 = playerFactory.resolve();
     this._player2 = rivalPlayerFactory.resolve();
-  }
-
-  private createPlayer(playerOptions: IPlayerOptionsV2, offsetX: number) {
-    const playerComputation = new PlayerComputation(
-      this.keyboard,
-      this.positionEmitter,
-      playerOptions
-    );
-    const player = new PlayerObject(
-      this.playerRenderer,
-      playerComputation,
-      playerOptions
-    );
-    player.position = playerOptions.position;
-    return player;
   }
 
   public addPlayers(appFactory: AppFactory) {
