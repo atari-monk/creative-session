cd87ac1855b6bf77ae373fb5506b4839260878ca
atari-monk <atari.monk1@gmail.com>
2023-07-25 21:37:05 +0200

    add log

--- 9-wood-stock-api/src/db.ts
+++ 9-wood-stock-api/src/db.ts
@@ -5,6 +5,7 @@ import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, './../.env') });
 
 const dbConnectionString = process.env.ATARI_MONK_WOOD_STOCK_API_DB;
+console.log('dbConnectionString:', dbConnectionString);
 if (!dbConnectionString) throw new Error('Database connection string not set!');
 
 mongoose.connect(dbConnectionString, {
