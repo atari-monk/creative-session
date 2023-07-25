17ced9299d1e649c727445a408a76ef6fd4cea63
atari-monk <atari.monk1@gmail.com>
2023-07-25 11:19:29 +0200

    upd script

--- 4-pixi/11-game-api-lib/script/build-lib.ps1
+++ 4-pixi/11-game-api-lib/script/build-lib.ps1
@@ -6,6 +6,11 @@ npm pack
 copy C:\atari-monk\Code\creative-session\4-pixi\11-game-api-lib\build\atari-monk-game-api-lib-0.0.1.tgz C:\atari-monk\Code\creative-session\4-pixi\2-pixi-lib
 cd C:\atari-monk\Code\creative-session\4-pixi\2-pixi-lib
 npm i atari-monk-game-api-lib-0.0.1.tgz
+#install in client
+copy C:\atari-monk\Code\creative-session\4-pixi\11-game-api-lib\build\atari-monk-game-api-lib-0.0.1.tgz C:\atari-monk\Code\creative-session\4-pixi\5-client
+cd C:\atari-monk\Code\creative-session\4-pixi\5-client
+npm i atari-monk-game-api-lib-0.0.1.tgz
 #clean files
 del C:\atari-monk\Code\creative-session\4-pixi\11-game-api-lib\build\atari-monk-game-api-lib-0.0.1.tgz
 del C:\atari-monk\Code\creative-session\4-pixi\2-pixi-lib\atari-monk-game-api-lib-0.0.1.tgz
+del C:\atari-monk\Code\creative-session\4-pixi\5-client\atari-monk-game-api-lib-0.0.1.tgz
--- 4-pixi/5-client/webpack.config.js
+++ 4-pixi/5-client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     extensions: ['.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'atari-monk-client.js',
     globalObject: 'this',
     library: {
