f8709e3a461b481bd9116ca5887d77624651187e
atari-monk <atari.monk1@gmail.com>
2023-07-25 11:06:11 +0200

    upd lib script

---
+++ 4-pixi/11-game-api-lib/script/build-lib.ps1
@@ -0,0 +1,11 @@
+npm i
+npm run build
+cd build
+npm pack
+#install in pixi-lib
+copy C:\atari-monk\Code\creative-session\4-pixi\11-game-api-lib\build\atari-monk-game-api-lib-0.0.1.tgz C:\atari-monk\Code\creative-session\4-pixi\2-pixi-lib
+cd C:\atari-monk\Code\creative-session\4-pixi\2-pixi-lib
+npm i atari-monk-game-api-lib-0.0.1.tgz
+#clean files
+del C:\atari-monk\Code\creative-session\4-pixi\11-game-api-lib\build\atari-monk-game-api-lib-0.0.1.tgz
+del C:\atari-monk\Code\creative-session\4-pixi\2-pixi-lib\atari-monk-game-api-lib-0.0.1.tgz
