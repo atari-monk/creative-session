801389475e97df2bf7cc2c55e3e7f78a603eea9e
atari-monk <atari.monk1@gmail.com>
2023-07-26 20:46:31 +0200

    add logs

--- 9-wood-api/src/db.ts
+++ 9-wood-api/src/db.ts
@@ -4,7 +4,13 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, './../.env') });
 
-const dbConnectionString = process.env.ATARI_MONK_WOOD_API_DB;
+console.log('Process Environment:', process.env);
+let dbConnectionString;
+try {
+  dbConnectionString = process.env.ATARI_MONK_WOOD_API_DB;
+} catch (error) {
+  console.log(error);
+}
 console.log('dbConnectionString:', dbConnectionString);
 if (!dbConnectionString) throw new Error('Database connection string not set!');
 
