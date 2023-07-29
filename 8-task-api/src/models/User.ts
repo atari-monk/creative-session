ddb3bc24f57e25057df3863d56ee1a96b413be2c
atari-monk <atari.monk1@gmail.com>
2023-07-29 17:12:20 +0200

    validate email

--- 8-task-api/src/models/User.ts
+++ 8-task-api/src/models/User.ts
@@ -1,10 +1,18 @@
 import mongoose, { Schema, Model } from 'mongoose';
 import { IUser } from './IUser';
+import validator from 'validator';
 
 const typeName = 'User';
 
 const userSchema: Schema<IUser> = new Schema<IUser>({
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: 'Invalid email format',
+    },
+  },
   maxRecords: { type: Number, required: true, default: 500 },
 });
 
